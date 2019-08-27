07b3431ae8d0aeb86395282f8e69230df52efdb1
Vladimir Sivcov <TheDeGe@yandex.ru>
2019-08-27 18:49:04 +0300

    Исправлены ошибки

--- face_checker/server.py
+++ face_checker/server.py
@@ -12,56 +12,21 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 MAX_DISTANCE = 0.5
 
 
-@app.route('/')
-def index():
-    return '''
-    <!doctype html>
-    <title>Upload Photo</title>
-    <h2>Upload Photo</h2>
-    <form action="/upload_image" method=post enctype=multipart/form-data>
-      <p><input type=file name=photo accept="image/*">
-         <input type=submit>
-    </form>
-    '''
-
-
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    photo = request.files['photo']
-
-    in_memory_file = io.BytesIO()
-    photo.save(in_memory_file)
-    image_data = np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
-    img = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
-
-    image_hash = hashlib.md5()
-    image_hash.update(img)
-    image_hash = image_hash.hexdigest()
-
-    face_location = face_recognition.face_locations(img)
-    face_encodings = face_recognition.face_encodings(img, face_location)
-
+def process_hashes_and_encodings(image_hash, face_encodings):
     if os.path.exists('db_hash_encoding.csv'):
         db_hash_encoding = pd.read_csv("db_hash_encoding.csv")
     else:
         db_hash_encoding = pd.DataFrame()
-
-    # print(db_hash_encoding)
 
     known_img_hashes, known_face_encodings = [], []
     for img_hash in db_hash_encoding.columns:
         known_img_hashes.append(img_hash)
         known_face_encodings.append(list(db_hash_encoding[img_hash]))
 
-    # print(known_img_hashes)
-    # print(known_face_encodings)
-
     db_hash_encoding[image_hash] = face_encodings[0]
     db_hash_encoding.to_csv('db_hash_encoding.csv', index=False)
 
     distances = face_recognition.face_distance(known_face_encodings, face_encodings[0])
-
-    # print(distances)
 
     rec_img_hashes = []
     if np.any(distances <= MAX_DISTANCE):
@@ -80,6 +45,43 @@ def upload_image():
     # else:
     #     cv2.imshow('photo', img)
     # cv2.waitKey()
+
+    return rec_img_hashes
+
+
+@app.route('/')
+def index():
+    return '''
+    <!doctype html>
+    <title>Upload Photo</title>
+    <h2>Upload Photo</h2>
+    <form action="/upload_photo" method=post enctype=multipart/form-data>
+      <p><input type=file name=photo accept="image/*">
+         <input type=submit>
+    </form>
+    '''
+
+
+@app.route('/upload_photo', methods=['POST'])
+def upload_photo():
+    photo = request.files['photo']
+
+    in_memory_file = io.BytesIO()
+    photo.save(in_memory_file)
+    image_data = np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
+    img = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
+
+    image_hash = hashlib.md5()
+    image_hash.update(image_data)
+    image_hash = image_hash.hexdigest()
+
+    face_location = face_recognition.face_locations(img)
+    face_encodings = face_recognition.face_encodings(img, face_location)
+
+    if face_encodings:
+        rec_img_hashes = process_hashes_and_encodings(image_hash, face_encodings)
+    else:
+        rec_img_hashes = 'Face not found'
 
     return jsonify(rec_img_hashes)
 
